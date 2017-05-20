aa5491a696ded1f48ece0883ae411fd62581b228
Ayc0 <benjamin.koltes@student.ecp.fr>
2017-05-20 12:26:18 +0200

    Feat: exported components in src/

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 // @flow
 import Expo from 'expo';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open main.js to start working on your app!</Text>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import App from './src/app';
 
 Expo.registerRootComponent(App);
